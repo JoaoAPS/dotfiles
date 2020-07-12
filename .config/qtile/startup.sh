9084759ce429e2cf6cc752e1e83f5b2fa80349df
João Paludo <jas.paludo@gmail.com>
2020-07-12 16:14:42 -0300

    Move startup from qtile/startup to xprofile

--- .config/qtile/startup.sh
+++ .config/qtile/startup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+#Veja também ~/.xprofile para programas rodados no startup
 
-exec picom &
-exec redshift -l -25.5:-50 -t 5500:3000 &
-exec numlockx off &
-exec dunst &
-exec unclutter -idle 5 &
+# exec picom &
+# exec redshift -l -25.5:-50 -t 5500:3000 &
+# exec numlockx off &
+# exec dunst &
+# exec unclutter -idle 5 &
 
 #Desabilita screensaver
-exec xset s off&
-exec xset s noblank&
-exec xset -dpms&
+# exec xset s off&
+# exec xset s noblank&
+# exec xset -dpms&
